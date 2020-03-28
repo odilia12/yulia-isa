2dc48e496e06f6403e7987b4bb8beb5a148bac44
odilia12 <40134338+odilia12@users.noreply.github.com>
2020-03-28 23:38:42 +0700

    Add files via upload (#2)

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ISA</title>
+</head>
+<body>
+<div>
+    <table>
+    	<thead>
+    		<tr>
+    			<td>Id</td>
+    			<td>Nama</td>
+    			<td>License</td>
+    		</tr>
+    	</thead>
+    	<tbody>
+    		<tr>
+    			<td>Id</td>
+    			<td>Nama</td>
+    			<td>License</td>
+    		</tr>    		
+    	</tbody>
+    </table>
+</div>
+
+</body>
+</html>
